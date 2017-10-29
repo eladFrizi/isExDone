a4c3d4bd3ed9869d262329d0708ffe084b0d65aa
eladFrizi <okc.elad.35@gmail.com>
2017-10-29 12:50:32 +0200

    check if its a folder of a student by if a the name contain a space

---
+++ missX.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+var exNo = process.argv[2];
+console.log('Checking Ex', exNo);
+// process.exit()
+
+
+fs.readdirSync('./').forEach(studentName => {
+    if (isStudentName(studentName)) {
+        // console.log('> ', studentName );
+        var isFolderValid = false
+        fs.readdirSync(`./${studentName}`).forEach(runnerFolder => {
+            if (runnerFolder === 'ExerciseRunner') {
+                
+                fs.readdirSync(`./${studentName}/ExerciseRunner`).for Each(exsFolder => {
+                    if (exsFolder === 'ex') {
+                        isFolderValid = true
+                        var exs = fs.readdirSync(`./${studentName}/ExerciseRunner/ex`)
+                        var isTargetDone = exs.includes(`${exNo}.js`);
+                        if (!isTargetDone) console.log(`${studentName} : ${isTargetDone}`);
+                    }
+                })
+            } 
+        })
+        if (!isFolderValid) console.log(`${studentName} have to fix his fodler`)
+    }
+})
+
+
+function isStudentName(str) {
+    //student !== 'app.js' && student !== 'tempCodeRunnerFile.js' && student !== 'test.txt'
+    return str.includes(' ')
+}
